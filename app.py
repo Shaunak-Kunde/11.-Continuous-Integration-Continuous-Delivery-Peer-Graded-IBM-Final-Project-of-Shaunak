ea51b861fca42eaf6946255f2b1184c6abb58336
SHAUNAK KUNDE <kundeshaunak@gmail.com>
2025-09-10 18:08:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+if __name__ == "__main__":
+    print("Simple Calculator App By Shaunak")
+    print("2 + 3 =", add(2, 3))
+    print("5 - 3 =", subtract(5, 3))
